5bf7af9b24713612032af5406c726c6eb8ad4e62
Marcia Zegarra <marcia.gzq@gmail.com>
2019-07-03 12:35:49 -0400

    Jquery library added

---
+++ main.js
@@ -0,0 +1,113 @@
+const gamestates = {
+    CHOOSEPLAYER: 'CHOOSEPLAYER',
+    CHOOSEDEFENDER: 'CHOOSEDEFENDER',
+    BATTLE: 'BATTLE',
+    VICTORY: 'VICTORY',
+    DEFEAT: 'DEFEAT',
+}
+
+var characters = [];
+var player = null;
+var defender = null
+var opponents = [];
+var gamestate = null
+
+function Initialize(){
+    gamestate = gamestates.CHOOSEPLAYER
+    characters = [
+        {
+            name: "Mario",
+            healthpoints: 120,
+            attackpower: 7,
+            baseattackpower: 7,
+            counterattack: 15,
+            src: "content/mario.png"
+        },
+        {
+            name: "Link",
+            healthpoints: 100,
+            attackpower: 12,
+            baseattackpower: 12,
+            counterattack: 6,
+            src: "content/link.png"
+        },
+        {
+            name: "Yoshi",
+            healthpoints: 80,
+            attackpower: 4,
+            baseattackpower: 4,
+            counterattack: 3,
+            src: "content/yoshi.png"
+        },
+        {
+            name: "Kirby",
+            healthpoints: 140,
+            attackpower: 10,
+            baseattackpower: 10,
+            counterattack: 5,
+            src: "content/kirby.png"
+        },        
+    ]
+    
+}
+
+function SelectPlayer(index){
+    player = characters[index];
+    opponents = characters.filter(function(character,i){return i!=index})
+    gamestate = gamestates.CHOOSEDEFENDER;
+}
+
+function SelectDefender(index){
+    defender = opponents[index];
+    gamestate = gamestates.BATTLE;
+}
+
+function ProcessFight(){
+    defender.healthpoints -= player.attackpower;
+    AddOutputLog("You attacked " + defender.name + " for " + player.attackpower + " damage")
+    if(defender.healthpoints > 0){
+        player.healthpoints -= defender.counterattack;
+        AddOutputLog(defender.name + " attacked you back for " + defender.counterattack + " damage")
+    }
+    player.attackpower += player.baseattackpower;
+    AnalyzeGameState()
+}
+
+function AnalyzeGameState(){
+    if(player.healthpoints <= 0){
+        gamestate = gamestates.DEFEAT;
+        AddOutputLog("You have been defeated");
+    }
+    if(defender.healthpoints <= 0){
+        AddOutputLog("You have defeated " + defender.name);
+        var remainingopponents = opponents.filter(function(opp){return opp.healthpoints > 0;});
+        if(remainingopponents.length == 0){
+            gamestate = gamestates.VICTORY;
+            AddOutputLog("You have defeated all of your opponents!!!");
+        }
+        else{
+            gamestate = gamestates.CHOOSEDEFENDER;
+            AddOutputLog("Choose your next opponent");
+        }
+    }
+}
+
+function ProcessGameState(){
+    switch(gamestate){
+        case 'CHOOSEPLAYER':
+            alert("Please choose a Character")
+            break;
+        case 'CHOOSEDEFENDER':
+            alert("Please choose a Defender")
+            break;
+        case 'BATTLE':
+            ProcessFight()
+            break;
+        case 'VICTORY':
+            alert("You have defeated all your opponents")
+            break;
+        case 'DEFEAT':
+            alert("You have been defeated")
+            break;
+    }
+}
